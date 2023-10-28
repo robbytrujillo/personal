febb371e082bd72e16da2bd62f6cbf536b5070da
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-10-28 11:18:51 +0700

    Personal | Profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,9 @@
     </head>
     <body >
         <div class="w-full h-screen grid grid-cols-4 bg-gray-500">
-            <div class="bg-red-500">Child component 1</div>
+            <div class="bg-white flex flex-col justify-center items-center">
+                <img class="w-[200px] h-[200px] bg-gray-500 rounded-full" />
+            </div>
             <div class="bg-blue-500">Child component 2</div>
             <div class="bg-green-500">Child component 3</div>
             <div class="bg-yellow-500">Child component 4</div>
